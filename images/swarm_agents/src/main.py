88b94f92d9d8f835e2f26482ccf061aa0ee7e1ef
Vishesh-Pandey <visheshpandey24@gmail.com>
2025-03-24 09:10:59 +0530

    deployed to lambda-works

--- images/swarm_agents/src/main.py
+++ images/swarm_agents/src/main.py
@@ -167,15 +167,29 @@ def display_messages(messages):
 
 def handler(event, context):
     data = event
+    try:
+        data = event.get('body') and json.loads(event['body']) or event
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": {"error": f"Invalid event == Unable to load data: {e}"}
+        }
     session_id = data['session_id']
     message = {
         "role": "user",
         "content": data['message']
     }
-    messages = get_messages(session_id)
-    messages.append({"role": "user", "content": message})
-    display_messages(messages)
-    save_messages_to_dynamodb(session_id=session_id, messages=[message], agent='Triage Agent') 
+    try:
+        messages = get_messages(session_id)
+        messages.append({"role": "user", "content": message})
+        display_messages(messages)
+        save_messages_to_dynamodb(session_id=session_id, messages=[message], agent='Triage Agent') 
+    except Exception as e:
+        return {
+            "statusCode": 400,
+            "body": {"error": f"Unable to access DynamoDB: {e}"}
+        }
+    
     response = run_demo_loop(session_id=session_id, debug=False)
 
     return {
--- lib/idocgen-stack.ts
+++ lib/idocgen-stack.ts
@@ -1,6 +1,7 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class IdocgenStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +13,27 @@ export class IdocgenStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'IdocgenQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+    const swarm_agents_function = new lambda.DockerImageFunction(
+      this,
+      "SwarmAgentsFunction",
+      {
+        code: lambda.DockerImageCode.fromImageAsset("./images/swarm_agents"),
+        memorySize: 1024,
+      }
+    );
+
+    const swarm_agents_url = swarm_agents_function.addFunctionUrl({
+      authType: lambda.FunctionUrlAuthType.NONE,
+      cors: {
+        allowedOrigins: ["*"],
+        allowedMethods: [lambda.HttpMethod.ALL],
+        allowedHeaders: ["*"],
+      },
+    });
+
+    new cdk.CfnOutput(this, "SwarmAgentsUrl", {
+      value: swarm_agents_url.url,
+    });
   }
 }
